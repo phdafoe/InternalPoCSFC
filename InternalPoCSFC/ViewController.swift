86336b0024fce01a1f019f6f735f1c163d37ab13
Phdafoe <phdafoe@gmail.com>
2022-03-23 12:41:14 +0100

    Initial Commit

---
+++ InternalPoCSFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternalPoCSFC
+//
+//  Created by TECDATA ENGINEERING on 23/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
