36fae68681217e8c09562809073d85233cc5c83c
Phdafoe <phdafoe@gmail.com>
2022-05-31 11:27:58 +0200

    [CICD] - test script

---
+++ Scripts.sh/test-deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Build packages
+pod install
+
+# Gitclone
+git clone https://github.com/phdafoe/InternalPoCSFC.git -b main -v
+
+# Fastlane
+export BUNDLE GEMFILE=$BUILD SCRIPTS DIR/Gemfile
+bundle install
+
+# Prepare, Build and Deploy
+bundle exec /usr/local/Cellar/fastlane/2.206.2/bin/fastlane scan
+bundle exec /usr/local/Cellar/fastlane/2.206.2/bin/fastlane gym
+
+# Run Specs (tests) if not confiqured to be skipped
+bundle exec /usr/local/Cellar/fastlane/2.206.2/bin/fastlane specs
+
+# Build IPA
+/usr/local/Cellar/fastlane/2.206.2/bin/fastlane build_ipa
+
+# Upload to APPCENTER
+/usr/local/Cellar/fastlane/2.206.2/bin/fastlane appcenter_release
